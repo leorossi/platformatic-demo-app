a164fe3c0f028faf36d0b1a27eaec430922a6290
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-12-07 17:26:50 +0100

    Adds simple route

--- plugin.js
+++ plugin.js
@@ -4,4 +4,7 @@
 /** @param {import('fastify').FastifyInstance} app */
 module.exports = async function (app) {
   console.log('The plugin has been loaded')
+  app.get('/hello', (req, res) => {
+    return res.send('Hello World!')
+  })
 }
